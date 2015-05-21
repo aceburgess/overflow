6664fb1866903f7c294bbade8ab829f55e5f9daf
fishermanng <sherman.ng10@gmail.com>
2015-05-21 18:03:04 -0400

    Add the vote migration

---
+++ db/migrate/20150521215614_create_votes.rb
@@ -0,0 +1,12 @@
+class CreateVotes < ActiveRecord::Migration
+  def change
+    create_table :votes do |t|
+      t.integer :user_id, null: false
+      t.integer :votable_id, null: false
+      t.string :votable_type, null: false
+      t.boolean :is_upvote, null: false
+
+      t.timestamps null: false
+    end
+  end
+end

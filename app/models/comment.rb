893ecb79e3d4edc55bdf38788b488e5eb25239c8
fishermanng <sherman.ng10@gmail.com>
2015-05-21 17:54:07 -0400

    Fix merge conflicts

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :questions, as: :commentable
   belongs_to :answers, as: :commentable
   belongs_to :users
+
+  validates :commentable, presence: true
+  validates :content, presence: true
+  validate :user_id, presence: true
 end

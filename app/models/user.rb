2cd3495b538e6b03f653ed87657425f74b6a725d
AlexTaber <alex.taber0@gmail.com>
2015-05-21 18:04:07 -0400

    add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
 
   validates :username, :email, :password_digest, presence: true
   validates :username, :email, uniqueness: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
 end
 
 

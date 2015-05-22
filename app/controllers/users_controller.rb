99974acd3f85b97323406bb31d11136aec38c189
AlexTaber <alex.taber0@gmail.com>
2015-05-22 11:56:27 -0400

    Users views implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,49 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(id: @user.id)
+    else
+      flash[:warn] = "That is not a valid user!"
+      redirect_to :back
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+
+    if @user.save
+      redirect_to user_path(id: @user.id)
+    else
+      flash[:warn] = "That is not a valid user!"
+      redirect_to :back
+    end
   end
 
   def destroy
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 end

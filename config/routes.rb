ffc90ad1a814231015f8f9657b0b2a90868b2c96
Ace Burgess <burgess.ace@gmail.com>
2015-05-22 11:39:52 -0400

    Update routes with a root to questions index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :comments, only: [:new, :create, :edit, :update, :destroy]
   resources :tags, only: [:new, :create, :destroy]
   resources :votes, only: [:new, :create, :edit, :update]
+  root 'questions#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

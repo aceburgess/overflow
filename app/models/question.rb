128567d0396b9d8e2314be9b749d3ccee7b69f46
Ace Burgess <burgess.ace@gmail.com>
2015-05-22 11:40:19 -0400

    Fix bug in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,10 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :votable
   has_many :answers
   belongs_to :user
-  has_one :chosen_answer, source: :answer
+  has_one :chosen_answer, class_name: :answer
 
   validates :title, presence: true
-  validates :content, presnece: true
+  validates :content, presence: true
   validates :user_id, presence: true
   validates :is_answered, presence: true
 end

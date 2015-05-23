4cf9bc03e07ec12813d6b8514474e98141f91732
AlexTaber <alex.taber0@gmail.com>
2015-05-23 19:08:16 -0400

    user graph added

--- app/assets/javascripts/user_graph.js
+++ app/assets/javascripts/user_graph.js
@@ -1,4 +1,32 @@
+
+
 function loadGApi(){
   google.load("visualization", "1", {packages:["corechart"]});
   google.setOnLoadCallback(setupChart);
+}
+
+function setupChart() {
+  chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
+  data = findData();
+  options = findOptions();
+  chart.draw(data, options);
+}
+
+function findOptions() {
+  return {
+    title: "Average Score for Questions/Answers",
+    seriesType: "bars",
+    colors: ["c0392b","c0392b"]
+  }
+}
+
+function findData(){
+  var data = new google.visualization.DataTable();
+  data.addColumn('string', 'Type');
+  data.addColumn('number', 'Average Score');
+  data.addRows([
+    ['Questions', -3],
+    ['Answers', 4],
+  ]);
+  return data
 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ActiveRecord::Base
   validates :username, :email, :password_digest, presence: true
   validates :username, :email, uniqueness: true
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, on: :create
+
+  def average_question_score
+    score_array = questions.map { |q| q.total_votes }
+    average = score_array.reduce(:+).to_f / score_array.size
+    average.nan? ? 0 : average
+  end
+
+  def average_answer_score
+    score_array = answers.map { |a| a.total_votes }
+    average = score_array.reduce(:+).to_f / score_array.size
+    average.nan? ? 0 : average
+  end
 end
 
 
